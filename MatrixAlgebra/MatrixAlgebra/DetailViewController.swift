fe579d7289169d21d14a331957f371d69c1a06d5
Tai Kao-Sowa <tkaosowa@stanford.edu>
2017-02-18 16:43:13 -0800

    Good table, Renner's add matrix button

--- MatrixAlgebra/MatrixAlgebra/DetailViewController.swift
+++ MatrixAlgebra/MatrixAlgebra/DetailViewController.swift
@@ -16,14 +16,20 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet
+    var tableView: UITableView!
+
     // Testing
-    let matrixData = [
+    var matrixData = [
         MatrixButton(name: "Matrix A", matrixParam: Matrix(row: 2, col: 2)),
         MatrixButton(name: "Matrix B", matrixParam: Matrix(row: 2, col: 3)),
         MatrixButton(name: "Matrix C", matrixParam: Matrix(row: 3, col: 3))
     ]
     
+    var matrixVector = [Matrix]()
+    
     @IBAction func determinantButton(_ sender: Any) {
+        print(matrixVector.count)
     }
     @IBAction func cofactorButton(_ sender: Any) {
     }
@@ -40,8 +46,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBAction func clearScreenButton(_ sender: Any) {
     }
     @IBAction func clearTableButton(_ sender: Any) {
+        print("Clear Table")
     }
     @IBAction func newMatrixButton(_ sender: Any) {
+        
+        //1. Create the alert controller.
+        let alert = UIAlertController(title: "New Matrix", message: "", preferredStyle: .alert)
+        
+        //2. Add the text fields. Name, Rows and Columns.
+        alert.addTextField { (Name) in
+            Name.text = "Name"
+        }
+        
+        alert.addTextField { (Rows) in
+            Rows.text = "Rows"
+        }
+        
+        alert.addTextField { (Columns) in
+            Columns.text = "Columns"
+        }
+        
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "Create", style: .default, handler: { [weak alert] (_) in
+            let name = alert?.textFields![0]
+            
+            let rowsString = alert?.textFields![1]
+            let rows = Int((rowsString?.text)!)
+            
+            let columnsString = alert?.textFields![2]
+            let columns = Int((columnsString?.text)!)
+            
+            let matrixAux = Matrix(row: rows!, col: columns!)
+            self.matrixVector.append(matrixAux)
+            
+        }))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+        
     }
     
     
@@ -50,10 +92,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let matrix1 = Matrix(row: 5, col: 2)
-        
-        print("Row: \(matrix1.row) Col: \(matrix1.col)")
-
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "matrixCell")
     }
     
     override func didReceiveMemoryWarning() {
@@ -62,14 +101,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     // Table functions
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0;
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func addMatrix(toAdd: Matrix) {
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.matrixData.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "matrixCell")! as UITableViewCell
+        let tempButton: MatrixButton = self.matrixData[indexPath.row]
+        let str1 = String(tempButton.row)
+        let str2 = String(tempButton.col)
+        
+        cell.textLabel?.text = tempButton.name
+        cell.detailTextLabel?.text = (str1 + " x " + str2)
+        
+        print(str1 + " x " + str2)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected matrix \(matrixData[indexPath.row].name)!")
+    }
+}
+
+extension Float {
+    var cleanValue: Float {
+        return self.truncatingRemainder(dividingBy: 1.0)
     }
 }
 
---
+++ MatrixAlgebra/MatrixAlgebra/MatrixTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MatrixTableViewCell.swift
+//  MatrixAlgebra
+//
+//  Created by Tai Kao-Sowa on 2/18/17.
+//  Copyright © 2017 Michael Karr. All rights reserved.
+//
+
+import UIKit
+
+class MatrixTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
